de97a4a1b3d80ddd5cc1014fe6a42b6604556437
John Li <john166@rocketmail.com>
2018-08-15 13:12:36 -0700

    Added more to help messages

--- discordTimeRanker.py
+++ discordTimeRanker.py
@@ -28,13 +28,27 @@ async def help(context):
     embeder = Embed(title='List of Commands', colour=26574, type='rich')
     embeder.add_field(name='~settup', value='Settup for ranks and commands'
                         + ' to change them')
+    embeder.add_field(name='~whitelist [discord_name#****]', value='Adds '
+                        + 'people to the whitelist. People on the whitelist '
+                        + 'are not ranked by time. Typing "~whitelist" by '
+                        + 'itself lists people on the list.')
+    embeder.add_field(name='~unwhitelist [discord_name#****]', value='Removes '
+                        + 'people from the whitelist. People removed will have '
+                        + 'their ranks set to the lowest level.')
     await bot.send_message(context.message.channel, embed=embeder)
     logger.debug(embeder.fields)
+
+#use time.time, then, in for loop, set delay to check, time.time - initial time.time
+#update experience when someone wants to check
 
 @bot.command(pass_context=True)
 async def settup(context):
     embeder = Embed(title='Rank Settup', colour=16777215, type='rich')
-    
+    embeder.add_field(name='~frequency -m/-h/-d [amount]', value='How often '
+                        + 'ranks will automatically update.\n-m/-h/-d denote ' 
+                        + 'how you want time to be measured.\n-m - minutes\n'
+                        + '-h - hours\n-d - days\n')
+    embeder.add_field(name='~update', value='Force update ranks.', inline=False)
     await bot.send_message(context.message.channel, embed=embeder)
     logger.debug(embeder.fields)
 
